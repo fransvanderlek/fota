c6732dcd2d0a87102cf0396035734010538c6804
fransvanderlek <fransvander@gmail.com>
2019-01-07 23:09:42 +0100

    working update run. needed to switch to local firmware store due to inexplicable read timeouts when using azure.

--- httpUpdate.ino
+++ httpUpdate.ino
@@ -13,14 +13,24 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
 
+#include <MQTT.h>
+#include <ArduinoJson.h>
+#include "config.h" //must create one
+
 #define USE_SERIAL Serial
-namespace {
 
-const int resetPin = 16;
+int updateFrequencySec = 1;
 
-}
+long lastMessageSentTimestamp;
 
-ESP8266WiFiMulti WiFiMulti;
+const int firmwareVersion = 1; // in each new firmware release, this version should be upped so we know not to upgrade after startup
+
+WiFiClientSecure espClient;
+MQTTClient iotHubMqttClient = MQTTClient(512); //really important to set buffer to sufficient number
+
+bool updating = false;
+String firmwareUrl = "";
+String urlThumbprint = "";
 
 void setup() {
 
@@ -41,44 +51,155 @@ void setup() {
 		delay(1000);
 	}
 
-	  WiFi.mode(WIFI_STA);
-	  WiFiMulti.addAP("koetjeloe", "ilhabela");
+	WiFi.mode(WIFI_STA);
 
-	  pinMode(16, OUTPUT);
-	  digitalWrite(16, HIGH);
+	WiFi.begin(secondary_ssid, secondary_password);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+	Serial.print("WiFi connected to ");
+	Serial.println(secondary_ssid);
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+
+	//setup iot hub MQTT transport
+	bool connect = false;
+	iotHubMqttClient.begin(iotHubDomain, 8883, espClient);
+	iotHubMqttClient.onMessage(messageReceived);
+
+	while (!connect) {
+		connect = iotHubMqttClient.connect(deviceId, mqttUserId, sasToken);
+
+		delay(1000);
+
+		Serial.print("last error status: ");
+		Serial.println(iotHubMqttClient.lastError());
+	};
+
+	//initTime();
+
+	iotHubMqttClient.subscribe(deviceBoundTopic); //works through device explorer and iot hub gui
+
+	//subscribe to properties update notifications
+
+	iotHubMqttClient.subscribe("$iothub/twin/PATCH/properties/desired/#");
+
+	//fetch initial state
+	iotHubMqttClient.subscribe("$iothub/twin/res/#");
+
+	iotHubMqttClient.publish("$iothub/twin/GET/?$rid={14545455}");
 
 }
 
 void loop() {
-	// wait for WiFi connection
-	if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-		Serial.println("Downloading firmware from https://cocostore.blob.core.windows.net/$web/hello_world.bin");
-		t_httpUpdate_return  ret = ESPhttpUpdate.update("https://cocostore.blob.core.windows.net/$web/hello_world.bin", "", "6a 81 2e f9 92 86 c1 48 4e 03 37 47 3a 2d 7d e3 26 dd 62 8b");
+	if (updating) {
 
-		//t_httpUpdate_return  ret = ESPhttpUpdate.update("https://server/file.bin");
+		t_httpUpdate_return ret;
+
+		if (urlThumbprint.length() > 0) {
+			Serial.println(urlThumbprint);
+			ret = ESPhttpUpdate.update(firmwareUrl, "", urlThumbprint);
+
+		} else {
+			ret = ESPhttpUpdate.update(firmwareUrl);
+
+		}
 
 		switch (ret) {
 		case HTTP_UPDATE_FAILED:
-			USE_SERIAL.printf("HTTP_UPDATE_FAILD Error (%d): %s",
+			Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s",
 					ESPhttpUpdate.getLastError(),
 					ESPhttpUpdate.getLastErrorString().c_str());
 
 			if (ESPhttpUpdate.getLastError() == 11) {
 				delay(2000);
-				digitalWrite(resetPin, LOW);
+				//digitalWrite(resetPin, LOW);
 			}
-
 			break;
 
 		case HTTP_UPDATE_NO_UPDATES:
-			USE_SERIAL.println("HTTP_UPDATE_NO_UPDATES");
+			Serial.println("HTTP_UPDATE_NO_UPDATES");
 			break;
 
 		case HTTP_UPDATE_OK:
-			USE_SERIAL.println("HTTP_UPDATE_OK");
+			Serial.println("HTTP_UPDATE_OK");
+			ESP.restart();
 			break;
 		}
+
 	}
+
+	iotHubMqttClient.loop();
+
+	delay(1000);
+	// wait for WiFi connection
+
+}
+
+void messageReceived(String &topic, String &payload) {
+	Serial.println("incoming: " + topic + " - " + payload);
+
+	StaticJsonBuffer<1024> jsonBuffer;
+	JsonObject& root = jsonBuffer.parseObject(payload);
+
+	String output;
+	root.printTo(output);
+
+	Serial.println(output);
+
+	int desiredUpdateFrequencySec;
+
+	int desiredFirmwareVersion;
+
+	if (topic.startsWith("$iothub/twin/res/")) {
+		Serial.println(" initial properties received");
+		Serial.println(
+				" reading  : "
+						+ root["desired"]["updateFrequencySec"].as<String>());
+		desiredUpdateFrequencySec =
+				root["desired"]["updateFrequencySec"].as<int>();
+
+		desiredFirmwareVersion = root["desired"]["firmwareVersion"].as<int>();
+		firmwareUrl = root["desired"]["firmwareUrl"].as<String>();
+		urlThumbprint = root["desired"]["urlThumbPrint"].as<String>();
+
+	} else if (topic.startsWith("$iothub/twin/PATCH/properties/desired/")) {
+		Serial.println(" update properties received");
+		Serial.println(
+				" reading  : " + root["updateFrequencySec"].as<String>());
+		desiredUpdateFrequencySec = root["updateFrequencySec"].as<int>();
+
+		desiredFirmwareVersion = root["firmwareVersion"].as<int>();
+
+		firmwareUrl = root["firmwareUrl"].as<String>();
+		urlThumbprint = root["urlThumbPrint"].as<String>();
+	}
+
+	if (desiredFirmwareVersion > firmwareVersion) {
+
+		Serial.println(
+				" Updating firmware to version  : " + desiredFirmwareVersion);
+		Serial.println(" download firmware from " + firmwareUrl);
+		updating = true;
+
+		//ESPhttpUpdate.setLedPin(ledPin, LOW);
+
+		//t_httpUpdate_return ret = ESPhttpUpdate.update(firmwareUrl, 0x00);
+
+//		Serial.println("Disconnecting MQTT");
+//		while (!iotHubMqttClient.disconnect()) {
+//			Serial.print(".");
+//
+//			delay(1000);
+//		}
+//		Serial.println(" Done.");
+
+
+
+	}
+
 }
 
